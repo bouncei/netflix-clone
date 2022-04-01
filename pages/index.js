526794c0a8f6a9f75d9ea71db1a38533f15aff57
bouncei <joshuainyang255@gmail.com>
2022-04-01 02:11:02 +0100

    fetch data about movies using the TMDB API

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 // import styles from '../styles/Home.module.css'
+import Row from "../components/Row";
+import requests from "./requests";
 
 export default function Home() {
   return (
     <div>
       <h1>NETFLIX CLONE</h1>
+
+      <Row title="Netflix Originals" fetchUrl={requests} />
+      <Row title="Trending Now" fetchUrl={requests} />
     </div>
-  )
+  );
 }
---
+++ pages/requests.js
@@ -0,0 +1,16 @@
+// Creating Axios for HTTP requests
+
+
+const API_KEY = "00ac511a8800f150d8edd280ac0a0884"
+
+
+
+const requests = {
+    // Insert all of the requests
+
+}
+
+
+
+export default requests
+
