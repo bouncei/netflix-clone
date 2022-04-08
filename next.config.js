2830365ae4c1c94598b31cf44cc496a2542ff273
bouncei <joshuainyang255@gmail.com>
2022-04-08 05:15:49 +0100

    environmental variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    // The Movie DataBase API KEY
+    API_KEY: "00ac511a8800f150d8edd280ac0a0884",
+  },
+};
