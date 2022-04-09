2dcca252a2468214ecdfc80250d8ba423931ceb0
bouncei <joshuainyang255@gmail.com>
2022-04-09 23:13:41 +0100

    commit

--- pages/requests.js
+++ pages/requests.js
@@ -10,6 +10,14 @@ const requests = {
   fetchHorrorMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=99`,
+
+  // more requests delete later
+  // fetchTopRated: `/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US`,
+  // fetchActionMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=28`,
+  // fetchComedyMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=35`,
+  // fetchHorrorMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=27`,
+  // fetchRomanceMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=10749`,
+  // fetchDocumentaries: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=99`,
 };
 
 console.log(requests.fetchActionMovies);
